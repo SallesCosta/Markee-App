8dd0f239cf70ebb82ce5561fcdbd9e50d2aca9c6
SallesCosta <newcapital.in@gmail.com>
2021-09-15 19:58:30 +0200

    em andamento

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,12 @@
-// import styled from "styled-components/macro"
+import styled from 'styled-components/macro'
 
-function App () {
-  return <h1>App</h1>
+function App() {
+  return <Title>App</Title >
 }
+
+const Title = styled.h1`${({ theme }) => `
+  background: ${theme.colors.black};
+  color: ${theme.colors.primary}
+`}`
 
 export { App }
--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from 'styled-components'
 import { App } from './app'
+import { theme } from './resource/theme'
 
-function Root () {
-  return <App />
+
+function Root() {
+  return (
+    <ThemeProvider theme={theme}>
+
+      <App />
+    </ThemeProvider>
+  )
 }
 
 export { Root }
