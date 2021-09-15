8ac35efac1d09b8b27a4bd57c0aebea56ce6e9ed
SallesCosta <newcapital.in@gmail.com>
2021-09-15 20:11:33 +0200

    em andamento

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
-import styled,  { css } from 'styled-components/macro'
+//import { ReactNode } from 'react'
+import styled, { css } from 'styled-components/macro'
 
-function App() {
+function App () {
   return <Title>App</Title>
 }
 
@@ -8,5 +9,13 @@ const Title = styled.h1`${({ theme }) => css`
   background: ${theme.colors.black};
   color: ${theme.colors.primary}
 `}`
+
+// type TitleProps = {
+//   children: ReactNode | ReactNode[]
+// }
+
+// function Title ({ children }: TitleProps) {
+//   return <h1>{children}</h1>
+// }
 
 export { App }
--- src/root.tsx
+++ src/root.tsx
@@ -1,11 +1,24 @@
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import { App } from './app'
 import { theme } from './resource/theme'
 
-function Root () {
+const GlobalStyle = createGlobalStyle`
+*{
+  box-sizing: border-box;
+}
+html {
+  font-size: 62.5%;  //essa configuração me garante que 1rem seja = a 10p x
+}
+
+body{
+  font-family: 'DM Sans', sans-serif;
+}
+ `
+
+function Root() {
   return (
     <ThemeProvider theme={theme}>
-
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   )
