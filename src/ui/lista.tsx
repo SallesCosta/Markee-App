e1cfd91d578eaf9c8331912b88d20d48845319fa
SallesCosta <newcapital.in@gmail.com>
2021-09-16 21:05:50 +0200

    chalange-5 em andamento

--- src/ui/lista.tsx
+++ src/ui/lista.tsx
@@ -1,30 +1,102 @@
 import styled, { css } from 'styled-components/macro'
 
-export function Lista () {
+// type Item = {
+//   id: string
+//   name: string
+//   content: string
+//   active: boolean
+//   status: 'editing' | 'saving' | 'saved'
+// }
+//  : Item
+
+export function Lista() {
   return (
     <ListaContainer>
       <ul>
-        <ListaInternal>
-          <li>
-            <a href='/'>README.md</a>
-            <span>x</span>
+        {data.map((item) => (
+          <li key={item.id}>
+            <a href={item.id}>{item.name}</a><span>x</span>
           </li>
-        </ListaInternal>
+        ))}
       </ul>
+      <ListaInternal />
     </ListaContainer>
   )
 }
 
-const ListaInternal = styled.li`${({ theme }) => css`
+const data = [
+  {
+    id: '1',
+    name: 'README.md',
+    content: '',
+    active: false,
+    status: 'saved',
+  },
+
+  {
+    id: '2',
+    name: 'CONTRIBUTING.md',
+    content: '',
+    active: true,
+    status: 'editing',
+  },
+
+  {
+    id: '3',
+    name: 'LICENSE.md',
+    content: '',
+    active: true,
+    status: 'saving',
+  },
+
+  {
+    id: '4',
+    name: 'Links.md',
+    content: '',
+    active: true,
+    status: 'saved',
+  },
+
+  {
+    id: '5',
+    name: 'roadmap.md',
+    content: '',
+    active: true,
+    status: 'editing',
+  },
+
+]
+
+const ListaInternal = styled.div`${({ theme }) => css`
 position: absolute;
 height: 37px;
 width: 268px;
 left: 32px;
 border-radius: 6px;
-background: rgba(255, 255, 255, 0.05);
-a, span{
-  color: ${theme.colors.white}
+background: rgba(255, 255, 255, 0.55);
+  a{
+    color: ${theme.colors.primaryDark}
+    font-family: 'DM Sans';
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 21px;
+    letter-spacing: -0.02em;
+    text-align: left;
+    margin-left: 50px;
 }
+
+  span{
+    color: ${theme.colors.white}
+   font-family: 'DM Sans';
+   font-size: 16px;
+   font-style: normal;
+   font-weight: 400;
+   line-height: 21px;
+   letter-spacing: -0.02em;
+   text-align: left;
+   margin-right: 0;
+  }
 `}`
 
 const ListaContainer = styled.div`
