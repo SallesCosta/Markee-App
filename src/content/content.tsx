6d74dfe333e64f294f931b6b2dca07edc8bc4f71
SallesCosta <newcapital.in@gmail.com>
2021-09-17 20:00:06 +0200

    chalange-5 em andamento-styling

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -5,7 +5,21 @@ export function Content() {
     <>
       <ContentInternal>
         <HeaderContent />
-      </ContentInternal>)
+        <SideUm />
+        <SideDois />
+        <FooterContent />
+      </ContentInternal>
+      )
+    </>
+  )
+}
+
+function HeaderContent() {
+  return (
+    <>
+      <HeaderContentInternal>
+        <input placeholder='CONTRIBUTE' />
+      </HeaderContentInternal>
     </>
   )
 }
@@ -18,17 +32,8 @@ width: 1588px;
 height: 100%;
 `}`
 
-function HeaderContent () {
-  return (
-    <>
-      <HeaderContentInternal>
-        <input placeholder='CONTRIBUTE' />
-      </HeaderContentInternal>
-    </>
-  )
-}
-
-const HeaderContentInternal = styled.header`
+const HeaderContentInternal = styled.header`${({ theme }) => css`
+  background: ${theme.colors.white};
   position: absolute;
   width: 100%;
   height: 100px;
@@ -38,5 +43,54 @@ const HeaderContentInternal = styled.header`
       margin-left: 60px;
       margin-top: 24px;
       border-radius: 0px;
+    }
+`}`
+
+function SideUm () {
+  return (
+    <>
+      <SideUmInternal>
+        <textarea value='side 1' />
+      </SideUmInternal>
+    </>
+  )
 }
+
+const SideUmInternal = styled.aside`
+background: #f7f7;
 `
+function SideDois () {
+  return (
+    <>
+      <SideDoisInternal>
+        <textarea value='side 2' />
+      </SideDoisInternal>
+    </>
+  )
+}
+
+const SideDoisInternal = styled.aside`
+    background: #f8f888;
+    width: 744px;
+    position: absolute;
+    height: 500px;
+`
+
+function FooterContent() {
+  return (
+    <>
+      <FooterInternal>
+        <p>feito com <strong>Digital</strong> por <strong>NewCapita.in</strong></p>
+      </FooterInternal>
+    </>
+  )
+}
+
+const FooterInternal = styled.footer`${({ theme }) => css`
+    background: ${theme.colors.primaryDark};
+    position: absolute;
+    width: 1588px;
+    height: 42px;
+    position: fixed;
+    bottom: 0;
+  `}`
