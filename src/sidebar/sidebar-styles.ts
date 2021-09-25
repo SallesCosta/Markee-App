de0b2467afa1ebb1ff18669396cfe29a082d274f
SallesCosta <newcapital.in@gmail.com>
2021-09-25 23:11:08 +0200

    função RemoveFile ok

--- src/sidebar/sidebar-styles.ts
+++ src/sidebar/sidebar-styles.ts
@@ -106,3 +106,44 @@ export const FileListItem = styled.li`${({ theme }) => css`
     }
   }
 `}`
+
+export const SidebarInternal = styled.aside`${({ theme }) => css`
+background: ${theme.colors.black};
+min-width: 332px;
+  max-width: 332px;
+  height:100%;
+  position: absolute;
+`}`
+
+export const Wrapper = styled.div`
+      margin-top: 100px;
+      padding: 32px;
+      `
+
+export const H2 = styled.h2`${({ theme }) => css`
+  color: ${theme.colors.white};
+  font-size: 1.6rem;
+  position: relative;
+  padding-left: 20px;
+  margin: 40px 0 32px;
+  span {
+    background-color: ${theme.colors.black};
+    display: inline-block;
+    position: relative;
+    padding: 0 10px;
+    z-index: 1;
+  }
+  &::before {
+    background: ${theme.colors.primary};
+    border-radius: 2px;
+    content: '';
+    display: block;
+    height: 2px;
+    left: 0;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 100%;
+    z-index: 0;
+  }
+`}`
--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,9 +1,8 @@
-import styled, { css } from 'styled-components/macro'
 import { BtnAdicionar } from '../ui/buttons'
 import { v4 as uuidv4 } from 'uuid'
 import { useState, RefObject } from 'react'
-import * as S from './sidebar-styles'
 import { HeaderSide } from './header-Sidebar'
+import * as S from './sidebar-styles'
 
 type SidebarProps = {
   inputRef: RefObject<HTMLInputElement>
@@ -45,11 +44,11 @@ export function Sidebar ({ inputRef }: SidebarProps) {
   }
 
   return (
-    <SidebarInternal>
+    <S.SidebarInternal>
       <BtnAdicionar onClick={AddNewFile}>+ Adicionar arquivo</BtnAdicionar>
       <HeaderSide />
-      <H2><span>Arquivos</span></H2>
-      <Wrapper>
+      <S.H2><span>Arquivos</span></S.H2>
+      <S.Wrapper>
         <S.FileList>
           {files.map(file => (
             <S.FileListItem key={(file.id)}>
@@ -67,49 +66,7 @@ export function Sidebar ({ inputRef }: SidebarProps) {
             </S.FileListItem>
           ))}
         </S.FileList>
-      </Wrapper>
-    </SidebarInternal>
+      </S.Wrapper>
+    </S.SidebarInternal>
   )
 }
-
-const SidebarInternal = styled.aside`${({ theme }) => css`
-background: ${theme.colors.black};
-min-width: 332px;
-  max-width: 332px;
-  height:100%;
-  position: absolute;
-
-`}`
-
-const Wrapper = styled.div`
-      margin-top: 100px;
-      padding: 32px;
-      `
-
-const H2 = styled.h2`${({ theme }) => css`
-  color: ${theme.colors.white};
-  font-size: 1.6rem;
-  position: relative;
-  padding-left: 20px;
-  margin: 40px 0 32px;
-  span {
-    background-color: ${theme.colors.black};
-    display: inline-block;
-    position: relative;
-    padding: 0 10px;
-    z-index: 1;
-  }
-  &::before {
-    background: ${theme.colors.primary};
-    border-radius: 2px;
-    content: '';
-    display: block;
-    height: 2px;
-    left: 0;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 100%;
-    z-index: 0;
-  }
-`}`
