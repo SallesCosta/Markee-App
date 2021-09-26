90b1325e527ebb2be26de156adcfb9e271a896c6
SallesCosta <newcapital.in@gmail.com>
2021-09-26 15:17:50 +0200

    hooks optimizations

--- src/app.tsx
+++ src/app.tsx
@@ -1,114 +1,15 @@
-import { useRef, useState, useEffect, ChangeEvent, MouseEvent } from 'react'
 import { Content } from './content/content'
-import { Sidebar, Files } from './sidebar/sidebar'
-import { v4 as uuidv4 } from 'uuid'
+import { Sidebar } from './sidebar/sidebar'
+import { useFiles } from './resource/hooks'
 
-function App () {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const [files, setFiles] = useState<Files[]>([])
-
-  useEffect(() => {
-    let timer: ReturnType<typeof setTimeout>
-
-    function updateStatus () {
-      const file = files.find(file => file.active === true)
-
-      if (!file || file.status !== 'editing') {
-        return
-      }
-
-      timer = setTimeout(() => {
-        setFiles(files => files.map(file => {
-          if (file.active) {
-            return {
-              ...file,
-              status: 'saving',
-            }
-          }
-
-          return file
-        }))
-
-        setTimeout(() => {
-          setFiles(files => files.map(file => {
-            if (file.active) {
-              return {
-                ...file,
-                status: 'saved',
-              }
-            }
-
-            return file
-          }))
-        }, 300)
-      }, 300)
-    }
-
-    updateStatus()
-
-    return () => clearTimeout(timer)
-  }, [files])
-
-  const AddNewFile = () => {
-    inputRef.current?.focus()
-
-    setFiles(files => files
-      .map(file => ({
-        ...file,
-        active: false,
-      }))
-      .concat({
-        id: uuidv4(),
-        name: 'Sem título',
-        content: '',
-        active: true,
-        status: 'saved',
-      }))
-  }
-
-  const RemoveFile = (id: string) => {
-    setFiles(prevState => prevState.filter(file => file.id !== id))
-    console.log('excluiu arquivo: ', id)
-  }
-
-  const handleUpdateFileName = (id: string) => (e: ChangeEvent<HTMLInputElement>) => {
-    setFiles(files => files.map(file => {
-      if (file.id === id) {
-        return {
-          ...file,
-          name: e.target.value,
-          status: 'editing',
-        }
-      }
-
-      return file
-    }))
-  }
-
-  const handleUpdateFileContent = (id: string) => (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setFiles(files => files.map(file => {
-      if (file.id === id) {
-        return {
-          ...file,
-          content: e.target.value,
-          status: 'editing',
-        }
-      }
-
-      return file
-    }))
-  }
-
-  const handleSelectFile = (id: string) => (e: MouseEvent) => {
-    e.preventDefault()
-
-    inputRef.current?.focus()
-
-    setFiles(files => files.map(file => ({
-      ...file,
-      active: file.id === id,
-    })))
-  }
+function App() {
+  const { files,
+    AddNewFile,
+    handleSelectFile,
+    RemoveFile,
+    inputRef,
+    handleUpdateFileName,
+    handleUpdateFileContent } = useFiles()
 
   return (
     <>
